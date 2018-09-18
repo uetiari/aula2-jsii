91e85051da9598887c877074dd6a80f2d72b7904
uetiari <arianeueti@gmail.com>
2018-09-18 16:38:14 -0300

    exercícios de aula

---
+++ 2/exemplo-focus/script/script.js
@@ -0,0 +1,64 @@
+const inputNome = document.getElementById("cadastroInputNome");
+const inputEmail = document.getElementById("cadastroInputEmail");
+const inputEmail2 = document.getElementById("cadastroInputEmailConfirm");
+const inputSenha = document.getElementById("cadastroInputPassword");
+const inputSenha2 = document.getElementById("cadastroInputPasswordConfirm");
+const inputTelefone = document.getElementById("cadastroInputPhone");
+const inputNews = document.getElementById("cadastroInputNews");
+
+const button = document.querySelector(".cadastro__button");
+
+const inputArea = document.getElementById("cadastroInputArea");
+const inputLevel = document.getElementsByName("level");
+
+button.addEventListener("click", function (evento){
+  evento.preventDefault();
+
+    const optionSelect = inputArea.options [inputArea.selectedIndex]
+    let radioItem;
+    for(let = i; i < inputLevel.length; i++) {
+      console.log(inputLevel[i])
+      radioItem = inputLevel [i]
+    }
+    
+  if (inputNome.value === "" || inputNome.value === " " || inputNome.value === undefined || inputNome.value === null) {
+    inputNome.focus();
+    return false
+  } 
+  else if (inputEmail.value === "" || inputEmail.value === " " || inputEmail.value === undefined || inputEmail.value === null) {
+    inputEmail.focus();
+    return false
+  }
+  
+  else if (inputEmail2.value === "" || inputEmail2.value === " " || inputEmail2.value === undefined || inputEmail2.value === null) {
+    inputEmail2.focus();
+    return false
+  }
+
+  else if (inputSenha.value === "" || inputSenha.value === " " || inputSenha.value === undefined || inputSenha.value === null){
+    inputSenha.focus();
+    return false
+  } 
+  else if (inputSenha2.value === "" || inputSenha2.value === " " || inputSenha2.value === undefined || inputSenha2.value === null){
+    inputSenha2.focus();
+    return false
+  } 
+  else if (inputTelefone.value === "" || inputTelefone.value === " " || inputTelefone.value === undefined || inputTelefone.value === null){
+    inputTelefone.focus();
+    return false
+  }
+  else if (inputEmail2.value !== inputEmail.value ){ //comparar se é diferente os e-mails e não deixa prosseguir
+    inputEmail.focus();
+    return false
+  } 
+  else if (inputSenha2.value !== inputSenha.value) {
+    alert("Senha diferente");
+    inputSenha.focus();
+    return false
+  }
+  else if (inputSenha.value.length < 7 ){
+    alert("Tem que ter 7 ou mais dígitos");
+    return false
+  } 
+ 
+})
