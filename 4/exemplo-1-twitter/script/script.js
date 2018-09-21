eb321edd9af581bce059bf3a27d9e1cc43053aef
uetiari <arianeueti@gmail.com>
2018-09-21 13:41:49 -0300

    adição do contador de caracteres

--- 4/exemplo-1-twitter/script/script.js
+++ 4/exemplo-1-twitter/script/script.js
@@ -1,15 +1,24 @@
 const button = document.querySelector(".tweet-composer__button"); //pega botão
+const tweet = document.getElementById("tweetComposerInput"); //pega caixa de ttexto
 
 button.addEventListener("click", function(event){
   event.preventDefault();
 
-  const tweet = document.getElementById("tweetComposerInput"); //pega caixa de ttexto
   const tweet2 = document.querySelector(".tweets-timeline"); //pega timeline do tweet
 
   // verifica se está vazio e não deixa mandar mensagem
   if(tweet.value === undefined || tweet.value === null || tweet.value === "" || tweet.value === " "){
     return false;
   }
+ 
+  // cria data automática
+  const date = new Date ();
+  const month = date.toLocaleDateString ("pt-BR", { //criação do mês escrito nome
+    month: "short" }); // forma que mostra o mês abreviado
+  const day = date.getDate();  // mostra o dia
+  const ano = date.getUTCFullYear(); // mostra o ano
+  const hora = date.getHours(); // mostra as horas 
+  const min = date.getMinutes(); //mostra minutos
 
   const div = document.createElement("div") // div que vai abraçar todo conteúdo
   div.className = "tweets-timeline__box"; // div que deixará tudo dentro do fundo branco
@@ -29,7 +38,7 @@ button.addEventListener("click", function(event){
   //insere header do html
   header.innerHTML = `<span class="tweets-timeline__name">Nome Sobrenome</span> 
   <span class="tweets-timeline__username">@username</span>
-  <span class="tweets-timeline__date">Dia de Mês</span>`
+  <span class="tweets-timeline__date">${day} de ${month} de ${ano} às ${hora}:${min}</span>`
   
   const footer = document.createElement("div"); //criei um div pra inserir footer
   footer.className = "tweets-timeline__footer";
@@ -45,7 +54,7 @@ button.addEventListener("click", function(event){
   div.appendChild(texto); //adiciona texto na div
   div.appendChild(footer); //adiciona footer na div
   
-  tweet2.insertBefore(div, tweet2.childNodes[0] ); // coloca na timeline semmpre antes do primeiro
+  tweet2.insertBefore(div, tweet2.childNodes[0] ); // coloca na timeline sempre antes do primeiro
   //sempre mais atual acima
 
   //para deixar campo de escrita limpo após enviar tweet
@@ -57,4 +66,37 @@ button.addEventListener("click", function(event){
       eventClear.preventDefault();
       div.remove(); // remover 
     })
+
+})
+
+//contador de caracteres
+tweet.addEventListener("keyup", function(){
+  const textBox = this;
+  const maxLength = 280;
+  const contador = document.getElementById ("tweetComposerCounter");
+
+  contador.innerHTML = maxLength - textBox.value.length; //vai contando os caracteres restantes
+
+  const caractDisp = maxLength - textBox.value.length;
+   // contador.innerHTML = caractDisp;
+
+   // se chegar os caracteres menor igual a 15
+  if (caractDisp <= 15){
+    //os caracteres restantes vão ficar vermelho
+    contador.style.color = "red";
+  } else {
+    // senão o contador continua branco
+    contador.style.color = "white";
+  }
+  // se o caracteres for menor que zero
+  if (caractDisp < 0 ) {
+    //desabilita o botão do tweet (usou td espaço dos caracteres)
+    button.disabled = true;
+  } else {
+    //senão ele continua habilitado  para postar 
+    button.disabled = false;
+  }
+  
+
+
 })
